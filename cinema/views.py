8ab065408d0daa204fdcd05e0f08fff365762a63
Vitalik <lg.mobile0808@gmail.com>
2025-10-07 00:41:27 +0300

    solut

--- cinema/views.py
+++ cinema/views.py
@@ -5,7 +5,6 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.pagination import PageNumberPagination
 from django.db.models import Q
 from rest_framework.pagination import BasePagination
-from rest_framework.response import Response
 
 from cinema.models import (
     Genre,
