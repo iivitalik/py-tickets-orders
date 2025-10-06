f9b84b9db970e7c3d6e2bfe49c2ac271728a8bbe
Vitalik <lg.mobile0808@gmail.com>
2025-10-06 20:01:37 +0300

    sol

--- cinema/views.py
+++ cinema/views.py
@@ -4,7 +4,6 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.pagination import PageNumberPagination
 from django.db.models import Q
-#f,f
 
 from cinema.models import (
     Genre,
