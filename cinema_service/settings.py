e91bd4e5c90fc48e406fbd26d8ea079615dc931e
Vitalik <lg.mobile0808@gmail.com>
2025-10-06 12:37:54 +0300

    solution

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -137,12 +137,3 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-REST_FRAMEWORK = {
-    "DEFAULT_FILTER_BACKENDS": [
-        "django_filters.rest_framework.DjangoFilterBackend"
-    ],
-    "DEFAULT_PAGINATION_CLASS":
-        "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 10
-}
