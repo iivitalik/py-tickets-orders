7120e5cabd6e1413395dba75f2906f35fe23a21b
Vitalik <lg.mobile0808@gmail.com>
2025-10-06 12:25:50 +0300

    solution

--- cinema/views.py
+++ cinema/views.py
@@ -1,6 +1,12 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+from django.db.models import Q
 
-from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession
+from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession, Order, Ticket
 
 from cinema.serializers import (
     GenreSerializer,
@@ -12,6 +18,10 @@ from cinema.serializers import (
     MovieDetailSerializer,
     MovieSessionDetailSerializer,
     MovieListSerializer,
+    OrderSerializer,
+    OrderCreateSerializer,
+    MovieSessionListWithTicketsSerializer,
+    MovieSessionDetailWithPlacesSerializer,
 )
 
 
@@ -56,3 +66,99 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
             return MovieSessionDetailSerializer
 
         return MovieSessionSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user).prefetch_related(
+            'tickets__movie_session__movie',
+            'tickets__movie_session__cinema_hall'
+        )
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return OrderCreateSerializer
+        return OrderSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    search_fields = ['title']
+    filterset_fields = ['genres', 'actors']
+
+    def get_queryset(self):
+        queryset = Movie.objects.all()
+
+        title = self.request.query_params.get('title')
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+
+        genres = self.request.query_params.get('genres')
+        if genres:
+            genre_list = genres.split(',')
+            queryset = queryset.filter(genres__name__in=genre_list).distinct()
+
+        actors = self.request.query_params.get('actors')
+        if actors:
+            actor_list = actors.split(',')
+            queryset = queryset.filter(
+                Q(actors__first_name__icontains=actor_list[0]) |
+                Q(actors__last_name__icontains=actor_list[0])
+            ).distinct()
+
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return MovieListSerializer
+        if self.action == "retrieve":
+            return MovieDetailSerializer
+        return MovieSerializer
+
+
+class MovieSessionViewSet(viewsets.ModelViewSet):
+    queryset = MovieSession.objects.all()
+    serializer_class = MovieSessionSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['movie']
+
+    def get_queryset(self):
+        queryset = MovieSession.objects.select_related(
+            'movie', 'cinema_hall'
+        ).prefetch_related('tickets')
+
+        date = self.request.query_params.get('date')
+        if date:
+            queryset = queryset.filter(show_time__date=date)
+
+        # Фільтрація за фільмом
+        movie_id = self.request.query_params.get('movie')
+        if movie_id:
+            queryset = queryset.filter(movie_id=movie_id)
+
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return MovieSessionListWithTicketsSerializer
+        if self.action == "retrieve":
+            return MovieSessionDetailWithPlacesSerializer
+        return MovieSessionSerializer
+
+    @action(detail=True, methods=['get'])
+    def taken_places(self, request, pk=None):
+        movie_session = self.get_object()
+        tickets = movie_session.tickets.all()
+        taken_places = [
+            {"row": ticket.row, "seat": ticket.seat}
+            for ticket in tickets
+        ]
+        return Response(taken_places)
