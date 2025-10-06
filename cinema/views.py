cf7dc23596ce750051287a2493c97d45cf165bb7
Vitalik <lg.mobile0808@gmail.com>
2025-10-06 19:23:14 +0300

    solution

--- cinema/views.py
+++ cinema/views.py
@@ -1,10 +1,8 @@
-from rest_framework import viewsets, status
+from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.filters import SearchFilter
 from django.db.models import Q
-from rest_framework.pagination import PageNumberPagination
 
 from cinema.models import (
     Genre,
